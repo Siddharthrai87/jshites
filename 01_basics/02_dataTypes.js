3bbd59949e5b45217e9023560e49bb8c6b20a244
Siddharthrai87 <raisk11112004@gmail.com>
2025-04-29 13:44:17 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" //treat all JS code as newer version
+
+// alert("Hello"); we are using nodejs not the browser 
+
+console.log(3+
+    3) //code readibility should be high
+console.log("Hitesh");
+
+// official documentation tc39.com,mdn
+
+// Data types
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+/*
+string =>""
+boolean =>true/false
+null=>standalone value , eg let stage = null
+undefined =>value not defined
+symbol => unique
+
+
+
+object 
+
+*/
+console.log(typeof"hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+// node 01_basics/02_dataTypes.js
