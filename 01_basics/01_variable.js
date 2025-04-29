cac2ebfdb1711eddbacb44561c40d476ba35d495
Siddharthrai87 <raisk11112004@gmail.com>
2025-04-29 13:19:50 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId=144553
+// constant declared only by const
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+console.log(accountState);
+
+
+// accountId=2; cannot be changed 
+accountEmail="hcc.com"
+accountPassword="431"
+accountCity="Delhi"
+
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// can be replaced by console .table 
+console.table([accountEmail,accountPassword,accountCity]);
