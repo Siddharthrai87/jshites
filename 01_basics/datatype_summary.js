78902bcef23d7b57b320558fe9cda982350f6fea
Siddharthrai87 <raisk11112004@gmail.com>
2025-05-03 10:40:56 +0000

    datatype_summart

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,30 @@
+// primitive data type
+// 7types : String , Number,Bollean, null , undefined ,Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLogin=false
+const outSide=null
+let userEmail;//undefined 
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id === anotherId);
+
+const bigNumber=207234857834787234n
+
+// non primitive data type or reference type
+//  Array , Objects , Functions
+
+const heros=["shaktimaan ","naagraj","doga"]
+let myObj={
+    name:"siddharth",
+    age:20
+}
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+myFunction();
+
+// node 01_basics/datatype_summary.js
