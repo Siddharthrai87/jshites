76ff02860c66f2e44291ef6483151299e3beff62
Siddharthrai87 <raisk11112004@gmail.com>
2025-05-03 10:13:56 +0000

    relational operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<0);
+
+// console.log(null>=0);//true  comparison convert null to a number , treating as zero
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// try to avoid such type of conversion
+
+// === strict check
+console.log("2"===2);
+// node 01_basics/04_comparison.js
