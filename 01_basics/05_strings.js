e784ae2bbad0dc1c3381213ada1e9a808a3aae76
Siddharthrai87 <raisk11112004@gmail.com>
2025-05-03 19:14:21 +0000

    string1

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name="hitesh"
+const repoCount=50
+
+// console.log(name+repoCount+"Value");//outdated syntax
+// console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);//string interpolation
+
+const gameName=new String('Hitesh-hc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2)); //character at the position
+
+// console.log(gameName.indexOf('t'));//position of the character
+
+// console.log(gameName.toUpperCase());
+const newString=gameName.substring(0,4)//cannot have negative indices
+console.log(newString);
+const anotherString=gameName.slice(-8,4)// can perform on negative indices
+console.log(anotherString);
+const newString1="   hitesh    "
+console.log(newString1);
+newString1.trim()
+console.log(newString1);
+console.log(newString1.trim());
+
+
+
+
+// node 01_basics/05_strings.js
