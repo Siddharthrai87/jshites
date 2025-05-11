da2f45dff8eebdbee78f597d7f1efbbf2b354068
Siddharthrai87 <raisk11112004@gmail.com>
2025-05-11 14:23:42 +0000

    array p1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,49 @@
+// Array part1
+
+// const myArr=[0,1,2,3,4,5,true,"Hitesh"];
+// console.log(myArr[6]);
+
+// declarations
+// const myHeros=["shaktimaan","naagraaj"]
+// const myArr2=new Array(1,2,3,4)
+
+// methods
+// myArr2.push(5);
+// console.log(myArr2);
+
+// myArr2.pop();
+// console.log(myArr2);
+
+// myArr2.unshift(9);
+// console.log(myArr2);
+
+// myArr2.shift();
+// console.log(myArr2);
+
+// console.log(myArr2.push(5));//5
+// console.log(myArr2.pop());//5
+// console.log(myArr2.pop());//4
+// console.log(myArr2.unshift(9));//4
+// console.log(myArr2.shift());//9
+
+// const myArr3=new Array(1,2,3,4,5,9)
+// console.log(myArr3.includes(9));//true
+// console.log(myArr3.indexOf(9));//5
+// console.log(myArr3.indexOf(7));//-1
+// const newArr=myArr3.join();
+// console.log(newArr);//1,2,3,4,5,9 is a string
+
+
+// const mynewArr1=[1,2,3,4,5,6];
+// console.log(mynewArr1);//[ 1, 2, 3, 4, 5, 6 ]
+// const slicedArr=mynewArr1.slice(2,4);
+// console.log(slicedArr);//[ 3, 4 ]
+// console.log(mynewArr1);//[ 1, 2, 3, 4, 5, 6 ]
+
+// const splicedArr=mynewArr1.splice(2,4);
+// console.log(splicedArr);//[ 3, 4, 5, 6 ]
+// console.log(mynewArr1);//[ 1, 2 ]
+
+
+
+// node 02_basics/01_array.js
